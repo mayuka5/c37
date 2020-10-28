8f8ce25ad9c61e1c969cf5d07eef04c969c8743b
mayuka5 <mymayuka2007@gmail.com>
2020-10-28 20:01:53 +0530

    game camera

--- sketch.js
+++ sketch.js
@@ -5,11 +5,13 @@ var playerCount;
 
 var database;
 
-var form, player, game;
+var form, player, game, allPlayers;
+
+var car1,car2,car3,car4,cars;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -18,4 +20,11 @@ function setup(){
 
 
 function draw(){
+  if(playerCount===4){
+    game.update(1);
+  }
+  if(gameState===1){
+    clear();
+    game.play();
+  }
 }
